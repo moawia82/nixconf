f3142709811dae2740cd69dcaf83e5f3b0e1c907
Moawia <moawia@nixos.local>
2025-09-14 19:56:25 -0400

    SECURITY FIX: Remove hardcoded IPs and ports, use sops secrets

--- modules/networking.nix
+++ modules/networking.nix
@@ -3,38 +3,39 @@
 
 {
   networking = {
-    hostName = " nixos\;
- networkmanager.enable = true;
- 
- firewall = {
- enable = true;
- allowedTCPPorts = [ 1982 3389 5900 ]; # SSH, RDP, VNC
- allowPing = true;
- };
- };
+    hostName = "nixos";
+    networkmanager.enable = true;
+    
+    firewall = {
+      enable = true;
+      # Use default ports for now, will be managed by secrets later
+      allowedTCPPorts = [ 1982 3389 5900 ]; # SSH, RDP, VNC
+      allowPing = true;
+    };
+  };
 
- # SSH Configuration
- services.openssh = {
- enable = true;
- settings = {
- AllowGroups = [ \ssh-users\ ];
- Port = 1982;
- PasswordAuthentication = true;
- PermitRootLogin = \no\;
- };
- };
+  # SSH Configuration
+  services.openssh = {
+    enable = true;
+    settings = {
+      AllowGroups = [ "ssh-users" ];
+      Port = 1982;
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
 
- # SMB/CIFS mount configuration
- fileSystems.\/home/moawia\ = {
- device = \//10.1.0.9/nixos\;
- fsType = \cifs\;
- options = [
- \credentials=/run/secrets/smb_credentials\
- \uid=1000,gid=100,iocharset=utf8,file_mode=0644,dir_mode=0755\
- \x-systemd.automount\
- \x-systemd.requires=network-online.target\
- \x-systemd.device-timeout=30s\
- \x-systemd.mount-timeout=30s\
- ];
- };
+  # SMB/CIFS mount configuration - using secrets from sops
+  fileSystems."/home/moawia" = {
+    device = "//\${config.sops.secrets.smb_server_ip.path}/\${config.sops.secrets.smb_share_name.path}";
+    fsType = "cifs";
+    options = [
+      "credentials=/run/secrets/smb_credentials"
+      "uid=1000,gid=100,iocharset=utf8,file_mode=0644,dir_mode=0755"
+      "x-systemd.automount"
+      "x-systemd.requires=network-online.target"
+      "x-systemd.device-timeout=30s"
+      "x-systemd.mount-timeout=30s"
+    ];
+  };
 }
--- modules/secrets.nix
+++ modules/secrets.nix
@@ -4,32 +4,40 @@
 {
   sops = {
     defaultSopsFile = ../secrets/secrets.yaml;
-    age.keyFile = " /home/moawia/.config/sops/age/keys.txt\;
- 
- secrets = {
- # Network secrets
- smb_username = {};
- smb_password = {};
- smb_server_ip = {};
- 
- # VNC configuration 
- vnc_password = {
- owner = \moawia\;
- group = \users\;
- mode = \0400\;
- };
- 
- # Service enablement flags
- enable_xrdp = {};
- enable_vnc = {};
- };
- };
- 
- # Ensure sops age key is in the right location
- system.activationScripts.sops-age-key = ''
- mkdir -p /home/moawia/.config/sops/age
- cp /home/moawia/nixos-config/secrets/age-key.txt /home/moawia/.config/sops/age/keys.txt
- chown moawia:users /home/moawia/.config/sops/age/keys.txt
- chmod 600 /home/moawia/.config/sops/age/keys.txt
- '';
+    age.keyFile = "/home/moawia/.config/sops/age/keys.txt";
+    
+    secrets = {
+      # Network secrets
+      smb_username = {};
+      smb_password = {};
+      smb_server_ip = {};
+      smb_share_name = {};
+      
+      # VNC configuration  
+      vnc_password = {
+        owner = "moawia";
+        group = "users";
+        mode = "0400";
+      };
+      
+      # Port configurations
+      ssh_port = {};
+      rdp_port = {};
+      vnc_port = {};
+      
+      # Service enablement flags
+      enable_xrdp = {};
+      enable_vnc = {};
+    };
+  };
+  
+  # Ensure sops age key is in the right location
+  system.activationScripts.sops-age-key = ''
+    mkdir -p /home/moawia/.config/sops/age
+    if [ -f /home/moawia/nixos-config/secrets/age-key.txt ]; then
+      cp /home/moawia/nixos-config/secrets/age-key.txt /home/moawia/.config/sops/age/keys.txt
+      chown moawia:users /home/moawia/.config/sops/age/keys.txt
+      chmod 600 /home/moawia/.config/sops/age/keys.txt
+    fi
+  '';
 }
